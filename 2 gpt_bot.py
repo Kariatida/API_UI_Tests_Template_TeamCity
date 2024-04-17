1e4d6f162341596179f29a6283a25c084d7f4ec5
Kariatida <52956332+Kariatida@users.noreply.github.com>
2024-04-16 21:48:15 -0400

    Update 2 gpt_bot.py

--- 2 gpt_bot.py
+++ 2 gpt_bot.py
@@ -56,3 +56,91 @@ def echo_all(message):
 print('start2')
 # Поллинг обновлений с использованием getUpdates
 bot.polling()
+
+import telebot
+import os
+
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import messages_from_dict, messages_to_dict
+from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = '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'
+tg_token = '6411150650:AAGp1_P9-alRuqh-U-V2neJ0h10SQXJ8eEs'
+
+bot = telebot.TeleBot(tg_token)
+
+template = """I want you to act as a mental health counselor. 
+I need your guidance and advice on managing emotions, stress, anxiety and 
+other mental health issues. You should use your knowledge of cognitive 
+behavioral therapy, meditation techniques, mindfulness practices, 
+and other therapeutic modalities to create strategies that a person 
+can implement to improve their overall well-being.
+First, try applying your knowledge of Stoicism.
+Maximum response length 5 sentences
+If possible, try to answer in 2-3 sentences.
+You need to involve the user in a dialogue
+Each sentence must be moved to a new line
+Answer in Russian
+
+{chat_history}
+Human: {human_input}
+Chatbot:"""
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"], template=template
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chain = LLMChain(
+    llm=ChatOpenAI(max_tokens=1500,temperature=0.7,model_name='gpt-3.5-turbo'),
+    prompt=prompt,
+    memory=memory,
+    verbose=True,
+)
+
+print('start1')
+
+import telebot
+import os
+
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import messages_from_dict, messages_to_dict
+from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain.chat_models import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = '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'
+tg_token = '6411150650:AAGp1_P9-alRuqh-U-V2neJ0h10SQXJ8eEs'
+
+bot = telebot.TeleBot(tg_token)
+
+template = """I want you to act as a mental health counselor. 
+I need your guidance and advice on managing emotions, stress, anxiety and 
+other mental health issues. You should use your knowledge of cognitive 
+behavioral therapy, meditation techniques, mindfulness practices, 
+and other therapeutic modalities to create strategies that a person 
+can implement to improve their overall well-being.
+First, try applying your knowledge of Stoicism.
+Maximum response length 5 sentences
+If possible, try to answer in 2-3 sentences.
+You need to involve the user in a dialogue
+Each sentence must be moved to a new line
+Answer in Russian
+
+{chat_history}
+Human: {human_input}
+Chatbot:"""
+
+prompt = PromptTemplate(
+    input_variables=["chat_history", "human_input"], template=template
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+llm_chain = LLMChain(
+    llm=ChatOpenAI(max_tokens=1500,temperature=0.7,model_name='gpt-3.5-turbo'),
+    prompt=prompt,
+    memory=memory,
+    verbose=True,
+)
+
+print('start1')
