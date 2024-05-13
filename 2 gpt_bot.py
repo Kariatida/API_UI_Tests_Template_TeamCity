962f9d094072235bb9d2ec9d00afcacd3249b162
Kariatida <52956332+Kariatida@users.noreply.github.com>
2024-05-12 23:46:51 -0400

    Update 2 gpt_bot.py

--- 2 gpt_bot.py
+++ 2 gpt_bot.py
@@ -81,7 +81,34 @@ from langchain import OpenAI, LLMChain, PromptTemplate
 from langchain.chat_models import ChatOpenAI
 
 os.environ["OPENAI_API_KEY"] = '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'
-tg_token = '6411150650:AAGp1_P9-alRuqh-U-V2neJ0h10SQXJ8eEs'
+tg_token = '6411150650:AAGp1_P9-alRuqh-U-V2neJ0h10SQXJ8eEscan implement to improve their overall well-being.
+First, try applying your knowledge of Stoicism.
+Maximum response length 5 sentences
+If possible, try to answer in 2-3 sentences.
+You need to involve the user in a dialogue
+Each sentence must be moved to a new line
+Answer in Russian
+
+{chat_history}
+print('start1')
+def send_to_gtp(message_text):
+    answer = llm_chain.predict(human_input=message_text)
+    return answer
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я бот на базе gpt")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    response = send_to_gtp(message.text)
+    bot.reply_to(message, response)
+print('start2')
+# Поллинг обновлений с использованием getUpdates
+bot.polling()
+
+import telebot
+import os'
 
 bot = telebot.TeleBot(tg_token)
 
